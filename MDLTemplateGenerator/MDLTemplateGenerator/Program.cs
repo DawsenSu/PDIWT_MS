c1b00a357a682d3549a6aa0a6ebb2d256bf1a0fc
edwinsu <1076083782@qq.com>
2017-08-01 17:06:22 +0800

    添加commmand line 生成不同功能

--- MDLTemplateGenerator/MDLTemplateGenerator/Program.cs
+++ MDLTemplateGenerator/MDLTemplateGenerator/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net.Sockets;
+using CommandLine;
+using CommandLine.Text;
 
 namespace MDLTemplateGenerator
 {
@@ -11,29 +14,67 @@ namespace MDLTemplateGenerator
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1 || !File.Exists(args.First()))
+            var options = new Options();
+            if (Parser.Default.ParseArguments(args,options))
             {
-                Console.WriteLine("项目路径错误");
-                return;
+                if (!File.Exists(options.VCProjectPath))
+                {
+                    Console.WriteLine($"无法打开VC项目文件：{options.VCProjectPath}");
+                    return;
+                }
+                if (options.IsOnlyMke)
+                {
+                    MakeFileGenerator fileGenerator = new MakeFileGenerator(options.VCProjectPath);
+                    fileGenerator.GenerateMakeFile();
+                    Console.WriteLine("mke文件生成");
+                }
+                else
+                {
+                    ResourceFileGenerator rscFileGenerator = new ResourceFileGenerator(options.VCProjectPath);
+                    rscFileGenerator.GenerateRscources();
+                    Console.WriteLine("资源文件生成");
+                    ClassFileGenerator classFileGenerator = new ClassFileGenerator(options.VCProjectPath);
+                    classFileGenerator.GenerateClasses();
+                    Console.WriteLine("程序文件生成");
+                    ModifyVCProjFile modifyVc = new ModifyVCProjFile(options.VCProjectPath);
+                    modifyVc.Modify();
+                    Console.WriteLine("VC工程文件修改完成");
+                    MakeFileGenerator fileGenerator = new MakeFileGenerator(options.VCProjectPath);
+                    fileGenerator.GenerateMakeFile();
+                    Console.WriteLine("mke文件生成");
+                }
             }
-            string vcprpjpath = args.First();
 
             //string vcprpjpath =
             //    @"D:\Programma\Source\C#\MDL\PDIWT_MS\PDIWT_MS\MDLTemplateGenerator\MDLTemplateGenerator\MDLTest\MDLTest\MDLTest.vcxproj";
-            ResourceFileGenerator rscFileGenerator = new ResourceFileGenerator(vcprpjpath);
-            rscFileGenerator.GenerateRscources();
+            //ResourceFileGenerator rscFileGenerator = new ResourceFileGenerator(vcprpjpath);
+            //rscFileGenerator.GenerateRscources();
 
-            ClassFileGenerator classFileGenerator = new ClassFileGenerator(vcprpjpath);
-            classFileGenerator.GenerateClasses();
+            //ClassFileGenerator classFileGenerator = new ClassFileGenerator(vcprpjpath);
+            //classFileGenerator.GenerateClasses();
 
-            ModifyVCProjFile modifyVc = new ModifyVCProjFile(vcprpjpath);
-            modifyVc.Modify();
+            //ModifyVCProjFile modifyVc = new ModifyVCProjFile(vcprpjpath);
+            //modifyVc.Modify();
 
-            MakeFileGenerator fileGenerator = new MakeFileGenerator(vcprpjpath);
-            fileGenerator.GenerateMakeFile();
+            //MakeFileGenerator fileGenerator = new MakeFileGenerator(vcprpjpath);
+            //fileGenerator.GenerateMakeFile();
 
-            Console.WriteLine("创建完成！");
+            //Console.WriteLine("创建完成！");
             //Console.ReadKey();
+        }
+    }
+
+    class Options
+    {
+        [Option('m',"only-mke", HelpText = "只根据VCProject生成MakeFile")]
+        public bool IsOnlyMke { get; set; }
+        [Option('f',"vcprojpath",HelpText = "VC工程文件的位置",Required = true)]
+        public string VCProjectPath { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, (current) => HelpText.DefaultParsingErrorsHandler(this, current));
         }
     }
 }
